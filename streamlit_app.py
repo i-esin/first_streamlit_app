838569df39379248ae1ef612f0540bf14e8f5937
Ivan Esin <146840048+i-esin@users.noreply.github.com>
2023-11-06 16:10:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,28 +23,49 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+
+
+
+
+
+
+
 # WS2 lesson 9
 def get_fruityvice_data(this_fruit_choice):
   fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+## New section to display ...
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#    streamlit.error('Please select a fruit to get information.')
+#  else:
+#    back_from_function = get_fruityvice_data(fruit_choice)
+#streamlit.dataframe(back_from_function)
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('fruitvice advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+
 
 
 streamlit.header("The fruit load list contains:")
 # Snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
     return my_cur.fetchall()
 
 # Add a button
