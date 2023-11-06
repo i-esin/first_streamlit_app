59feb6cdf18c591ee61e51f460278c0a1d7b91ac
Ivan Esin <146840048+i-esin@users.noreply.github.com>
2023-11-06 16:13:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,32 +32,33 @@ streamlit.dataframe(fruits_to_show)
 
 
 # WS2 lesson 9
+# Create repeatable code block
 def get_fruityvice_data(this_fruit_choice):
   fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-## New section to display ...
-#streamlit.header("Fruityvice Fruit Advice!")
-#try:
-#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#  if not fruit_choice:
-#    streamlit.error('Please select a fruit to get information.')
-#  else:
-#    back_from_function = get_fruityvice_data(fruit_choice)
-#streamlit.dataframe(back_from_function)
-
-streamlit.header('fruitvice advice')
+# New section to display ...
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+
+#streamlit.header('fruitvice advice')
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#    streamlit.error('Please select a fruit to get information.')
+#  else:
+#    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+#    streamlit.dataframe(fruityvice_normalized)
+#except URLError as e:
+#  streamlit.error()
 
 
 
